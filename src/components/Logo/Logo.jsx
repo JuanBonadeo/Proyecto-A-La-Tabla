a385f518f68bbed80b955e22c57972a7eab65733
Juan Cruz <juancruzbonadeo04@gmail.com>
2024-03-21 19:55:33 -0300

    logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-      <Link to="/"><img className="logo" src="logo.PNG" alt="A la tabla logo" /></Link>
+      <Link to="/"><img className="logo" src="logo.png" alt="A la tabla logo" /></Link>
   );
 };
 
